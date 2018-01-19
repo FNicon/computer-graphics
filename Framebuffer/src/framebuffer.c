a79a6fb18812c59436df1805aa27a9a38d87fb0a
Reinhard Linardi <reinhardlinardi@gmail.com>
2018-01-19 20:49:35 +0700

    Add original example, move previous to src

--- Framebuffer/src/framebuffer.c
+++ Framebuffer/src/framebuffer.c
@@ -1,3 +1,4 @@
+
 /*
 To test that the Linux framebuffer is set up correctly, and that the device permissions
 are correct, use the program below which opens the frame buffer and draws a gradient-
@@ -61,25 +62,25 @@ int main()
     x = 100; y = 100;       // Where we are going to put the pixel
 
     // Figure out where in memory to put the pixel
-    for (y = 100; y < 300; y++)
-        for (x = 100; x < 300; x++) {
+    for (y = 4; y < 14; y++)
+        for (x = 0; x < 8; x++) {
 
             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                        (y+vinfo.yoffset) * finfo.line_length;
 
             if (vinfo.bits_per_pixel == 32) {
-                *(fbp + location) = 100;        // Some blue
-                *(fbp + location + 1) = 15+(x-100)/2;     // A little green
-                *(fbp + location + 2) = 200-(y-100)/5;    // A lot of red
+                *(fbp + location) = 255;        // Some blue
+                *(fbp + location + 1) = 255;     // A little green
+                *(fbp + location + 2) = 255;    // A lot of red
                 *(fbp + location + 3) = 0;      // No transparency
         //location += 4;
-            } else  { //assume 16bpp
-                int b = 10;
-                int g = (x-100)/6;     // A little green
+            } /*else  { //assume 16bpp
+                int b = 255;
+                int g = 255;     // A little green
                 int r = 31-(y-100)/16;    // A lot of red
                 unsigned short int t = r<<11 | g << 5 | b;
                 *((unsigned short int*)(fbp + location)) = t;
-            }
+            }*/
 
         }
     munmap(fbp, screensize);
