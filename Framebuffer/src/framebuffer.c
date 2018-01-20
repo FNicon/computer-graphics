4e5bf86b696aa05e4ff1f0efc84cefd0c96af25b
Reinhard Linardi <reinhardlinardi@gmail.com>
2018-01-20 10:25:00 +0700

    Add validation

--- Framebuffer/src/framebuffer.c
+++ Framebuffer/src/framebuffer.c
@@ -11,8 +11,9 @@
 /* ---- CONSTANTS ----- */
 
 #define LETTER_NUMBER 26  // first LETTER_NUMBER letters which pixels are defined in ../font/font.txt
-#define ROW_NUMBER 10  // row size of every letter
-#define COL_NUMBER 7  // col size of every letter
+#define ROW_NUMBER 10  // height of every letter in pixels
+#define COL_NUMBER 7  // width of every letter in pixels
+#define COL_SPACING 1  // spacing between two sequential letters in pixels
 
 #define FILE_NAME "font.txt" // font pixels configuration file name
 #define FILE_PATH "../font/" // folder where font pixels configuration file is saved
@@ -34,20 +35,20 @@ int main()
     // Open the file for reading and writing
     fbfd = open("/dev/fb0", O_RDWR);
     if (fbfd == -1) {
-        perror("Error: cannot open framebuffer device");
+        perror("Error! Cannot open framebuffer device");
         exit(1);
     }
     //printf("The framebuffer device was opened successfully.\n");
 
     // Get fixed screen information
     if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
-        perror("Error reading fixed information");
+        perror("Error! Cannot read fixed information");
         exit(2);
     }
 
     // Get variable screen information
     if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
-        perror("Error reading variable information");
+        perror("Error! Cannot read variable information");
         exit(3);
     }
 
@@ -59,7 +60,7 @@ int main()
     // Map the device to memory
     fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
     if ((int)fbp == -1) {
-        perror("Error: failed to map framebuffer device to memory");
+        perror("Error! Failed to map framebuffer device to memory");
         exit(4);
     }
     //printf("The framebuffer device was mapped to memory successfully.\n");
@@ -74,11 +75,10 @@ int main()
     strcat(path, FILE_NAME);
 
     file = fopen(path, "r");
+    bool pix[LETTER_NUMBER][ROW_NUMBER][COL_NUMBER]; // store font pixels, pix[0][1][2] = 1st letter (A), 2nd row, 3rd column, true/false = pixel on/off
 
     if(file)
     {
-        bool pix[LETTER_NUMBER][ROW_NUMBER][COL_NUMBER]; // store font pixels, pix[0][1][2] = 1st letter (A), 2nd row, 3rd column, true/false = pixel on/off
-        
         int letter, row, col; // iterators
         char c;
 
@@ -102,34 +102,56 @@ int main()
     }
     else
     {
-        perror("Error: failed to open file");
+        perror("Error! Failed to open pixels configuration file");
         exit(5);
     }
 
 
-    /* --------------- READ INPUT --------------- */
+    /* --------------- READ INPUT AND VALIDATE --------------- */
 
-    printf("----- CUSTOM FONT RENDERER -----\n\n");
+    printf("\n----- CUSTOM FONT RENDERER -----\n\n");
     printf("Want to see our font? Check it out now!\nType anything below without whitespace, A-Z only, and max length is 50.\n\n");
     printf("Input : ");
 
     char in[50]; // user's input
     scanf("%s",in);
 
-    printf("%s\n",in);
+    int len = strlen(in); // get input length
+    int pos; // iterator
+    
+    if(len > 50) 
+    {
+        printf("\nError! Input too long\n");
+        exit(6);
+    }
+    else
+    {
+        for(pos = 0; pos < len; pos++)
+        {
+            if(in[pos] >= 'A' && in[pos] <= 'Z') continue;
+            else if(in[pos] >= 'a' && in[pos] <= 'z') in[pos] = toupper(in[pos]);
+            else
+            {
+                printf("\nError! Invalid input\n");
+                exit(7);
+            }
+        }
+    }
 
+    /* --------------- FONT RENDERING --------------- */
 
-    /* --------------- RENDERING --------------- */
+    int letter, row, col; // iterators
 
-    x = 100; y = 100; // Where we are going to put the pixel
+    int max_row = ROW_NUMBER - 1; // maximum value of row = ROW_NUMBER - 1, because one line only and number starts from 0
+    int max_col = len * COL_NUMBER + (len-1) * COL_SPACING - 1; // maximum value of col, depends on input length, number starts from 0
 
     // Figure out where in memory to put the pixel
-    for(y = 4; y < 14; y++)
+    for(row = 0; row < max_row; row++)
     {
-        for(x = 0; x < 8; x++)
+        for(col = 0; col < 1; col++)
         {
-            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                       (y+vinfo.yoffset) * finfo.line_length;
+            location = (col + vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+                       (row + vinfo.yoffset) * finfo.line_length;
 
             *(fbp + location) = 255; // Blue value
             *(fbp + location + 1) = 255; // Green value
