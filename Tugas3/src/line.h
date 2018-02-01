0eb00f719931e7b88831d61c24d52eeee59d077d
FNicon <mybeddrom@gmail.com>
2018-01-31 18:05:19 -0800

    Add Line Algorithm

--- Tugas3/src/line.h
+++ Tugas3/src/line.h
@@ -2,18 +2,21 @@
 
 #include "framebuffer.h"
 
-class Line
-{
-    public:
-        // Properties
+class Line {
+	public:
+		// Properties
 
-        int x1, y1; // First point coordinate
-        int x2, y2; // Second point coordinate
+		int x1, y1; // First point coordinate
+		int x2, y2; // Second point coordinate
 
-        // Methods
+		// Methods
 
-        Line(int _x1, int _y1, int _x2, int _y2); // Line constructor
+		Line(int _x1, int _y1, int _x2, int _y2); // Line constructor
 
-        // Draw line to main buffer or back buffer with RGB color, use hex for color
-        void draw(Framebuffer& buf, int color, bool main);
+		// Draw line to main buffer or back buffer with RGB color, use hex for color
+		void draw(Framebuffer& buf, int color, bool main);
+
+		void drawHigh (Framebuffer& buf, int color, bool main);
+
+		void drawLow (Framebuffer& buf, int color, bool main);
 };
--- Tugas3/src/main.cpp
+++ Tugas3/src/main.cpp
@@ -7,42 +7,44 @@
 
 using namespace std;
 
-int main()
-{
-    Framebuffer buf;
+int main(){
+	Framebuffer buf;
 
-    int x1 = 2;
-    int y1 = 2;
-    int x2 = 100;
-    int y2 = 100;
+	int x1 = 0;
+	int y1 = 0;
+	int x2 = 100;
+	int y2 = 100;
+	int phase = 0;
 
-    Line line(x1, y1, x2, y2);
-    Line line2(100, 0, 100, 100);
-    Line line3(100, 100, 200, 0);
-    Line line4(100, 100, 200, 100);
-    Line line5(100, 100, 200, 200);
-    Line line6(100, 100, 100, 200);
-    Line line7(100, 100, 0, 200);
-    Line line8(100, 100, 0, 100);
-    int cnt = 400;
+	Line line(x1, y1, x2, y2);
+	int cnt = 800;
 
-    /*while(cnt--)
-    {*/
-        //line.draw(buf, 0x5342f4, buf.BUF_MAIN);
-        //line2.draw(buf, 0x5342f4, buf.BUF_MAIN);
-        //line3.draw(buf, 0x5342f4, buf.BUF_MAIN);
-        //line4.draw(buf, 0x5342f4, buf.BUF_MAIN);
-        //line5.draw(buf, 0x5342f4, buf.BUF_MAIN);
-        //line6.draw(buf, 0x5342f4, buf.BUF_MAIN);
-        //line7.draw(buf, 0x5342f4, buf.BUF_MAIN);
-        line8.draw(buf, 0x5342f4, buf.BUF_MAIN);
+	while(cnt--) {
+	line.draw(buf, 0x5342f4, buf.BUF_BACK);
+	if (phase == 0) {
+		line.x1 = line.x1 + 1;
+		if (line.x1 >= 200) {
+			phase = 1;
+		}
+	} else if (phase == 1) {
+		line.y1 = line.y1 + 1;
+		if (line.y1 >= 200) {
+			phase = 2;
+		}
+	} else if (phase == 2) {
+		line.x1 = line.x1 - 1;
+		if (line.x1 <= 0) {
+			phase = 3;
+		}
+	} else if (phase == 3) {
+		line.y1 = line.y1 -1;
+		if (line.y1 <= 0) {
+			phase = 0;
+		}
+	}
+	buf.flush();
+	usleep(10000);
+	}
 
-        //line.x1++; line.y1++;
-        //line.x2++; line.y2++;
-
-        //buf.flush();
-        /*usleep(10000);
-    }*/
-
-    return 0;
+	return 0;
 }
